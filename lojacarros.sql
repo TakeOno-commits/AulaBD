c3902f950d8bbe727987719c1907733c01738afa
wagnermarques <wagnerdocri@gmail.com>
2026-06-16 20:39:09 -0300

    exercicioBD_16-06

--- lojacarros.sql
+++ lojacarros.sql
@@ -1,26 +1,44 @@
-drop database carrosdb;
-create database carrosdb;
-use carrosdb;
-create table clientes(
-    id int primary key auto_increment,
-    nome varchar(100) not null
+DROP DATABASE IF EXISTS carrosdb;
+CREATE DATABASE carrosdb;
+USE carrosdb;
+
+CREATE TABLE clientes(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL
 );
-create table produtos(
-    id int primary key auto_increment,
-    nome varchar(100) not null,
-    pco float
+
+CREATE TABLE produtos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    pco FLOAT
 );
-create table vendas(
-    id int primary key auto_increment,
-    cli_id int,
-    prod_id int      
+
+CREATE TABLE vendas(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    cli_id INT,
+    prod_id INT,
+
+    CONSTRAINT fk_cli_id FOREIGN KEY (cli_id) REFERENCES clientes(id),
+
+    CONSTRAINT fk_prod_id FOREIGN KEY (prod_id) REFERENCES produtos(id)
 );
- 
-ALTER TABLE vendas ADD CONSTRAINT fk_cli_id FOREIGN KEY (cli_id) REFERENCES clientes(id);
- 
-insert into clientes (nome) value ('Claude Shannon');
-insert into produtos (nome) value ('Monza');
-insert into vendas (cli_id, prod_id) values (1,1);
-insert into vendas (cli_id, prod_id) values (50,50);
- 
-select * from vendas;
+
+-- CLIENTES
+INSERT INTO clientes (nome) VALUES ('Claude Shannon');
+INSERT INTO clientes (nome) VALUES ('Alan Turing');
+INSERT INTO clientes (nome) VALUES ('Ada Lovelace');
+
+-- PRODUTOS
+INSERT INTO produtos (nome, pco) VALUES ('Monza', 50000);
+INSERT INTO produtos (nome, pco) VALUES ('Fusca', 35000);
+INSERT INTO produtos (nome, pco) VALUES ('Gol', 42000);
+
+-- VENDAS
+INSERT INTO vendas (cli_id, prod_id) VALUES (1,1);
+INSERT INTO vendas (cli_id, prod_id) VALUES (2,2);
+INSERT INTO vendas (cli_id, prod_id) VALUES (3,3);
+
+-- CONSULTAS
+SELECT * FROM clientes;
+SELECT * FROM produtos;
+SELECT * FROM vendas;
